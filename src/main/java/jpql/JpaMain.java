a43588924926a62fb40658d5cada5e72f9aa1b39
akdgns12 <akdgns12@naver.com>
2022-02-24 13:46:54 +0900

    JPQL 기본문법과 쿼리API

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -21,7 +18,26 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+            //TypedQuery : 반환타입이 명활할 떄 사용 ex) query1 - Member.class, query2 - String
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            //Query : 반환타입이 명확하지 않을 떄 사용 ex) query3 - String, int
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
+
+            //query.getResultList() : 결과가 하나 이상일 떄, 리스트 반환
+            //결과가 없으면 빈 리스트 반환
+            //query.getSingleResult() : 결과가 정확히 하나, 단일 객체 반환
+            //결과가 없으면 : NOResultException
+            //결과가 둘 이상이면 : NonUniqueExceResultption
+
+            //파라미터 바인딩 - 이름기준, 위치기준  BUT 위치기준으로는 쓰지말고 이름기준으로만 사용하자
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e) {
